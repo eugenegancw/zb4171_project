c25f9ff3e2464a446b23785172faa17a17a13e70
Eugene Gan <91330917+eugenegancw@users.noreply.github.com>
2022-11-15 01:08:55 +0800

    Add files via upload

--- clusterProfilerCode/clusterProfiler.R
+++ clusterProfilerCode/clusterProfiler.R
@@ -12,14 +12,14 @@ signif<-model.results
 signif.entrez<-unique(signif$SYMBOL)
 
 #Analyse using custom_BP
-custom_BP <-read.gmt("../data/go-bp_2021_gene_set.gmt")
+custom_BP <-read.gmt("../customGMT/go-bp_2021_gene_set.gmt")
 custom_BP.gene_symbol<- select(custom_BP, term, gene)
 enrich.GO<- enricher(gene=signif.entrez, pvalueCutoff=1, TERM2GENE = custom_BP.gene_symbol)
 head(enrich.GO@result)
 write.csv(enrich.GO@result,"brca_clusterProfiler_output/brca_cluseterProfiler_BP_custom_noThreshold.csv", row.names = FALSE)
 
 #Analyse using custom_MF
-custom_MF <-read.gmt("../data/go-mf_2021_gene_set.gmt")
+custom_MF <-read.gmt("../customGMT/go-mf_2021_gene_set.gmt")
 custom_MF.gene_symbol<- select(custom_MF, term, gene)
 #note that for our project, we output geneset lists regardless of significance level
 enrich.GO<- enricher(gene=signif.entrez, pvalueCutoff=1, TERM2GENE = custom_MF.gene_symbol)
